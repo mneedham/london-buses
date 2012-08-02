45e771a46aa4c291dbc61f1c4a54e14958d9b302
Mark Needham <m.h.needham@gmail.com>
2012-08-03 00:15:18 +0100

    mark: adding a custom lucene index so I can query the stops using cypher

--- data/load.rb
+++ data/load.rb
@@ -1,19 +1,9 @@
 require 'rubygems'
-require 'neography'
 require 'fastercsv'
 require 'open-uri'
 require 'json'
 
 require 'neo4j'
-
-class Stop
-  include Neo4j::NodeMixin
-  index :code
-  property :code
-  property :name
-end
-
-neo = Neography::Rest.new
 
 data_dir = File.expand_path('data') + '/'
 
@@ -29,15 +19,46 @@ stops.each do |stop|
 	northing = stop[5]
 
 	stops_to_add << { :name => name, :code => code  }
-	puts "Code: #{code}, Stop: #{name}"
 end
 
 Neo4j::Config[:storage_path] = File.expand_path('neo4j') + "/data/graph.db"
 
+class StopsIndex
+  extend Neo4j::Core::Index::ClassMethods
+  include Neo4j::Core::Index
+
+  self.node_indexer do
+    index_names :exact => 'stops'
+    trigger_on :type => "stop"
+  end
+
+  index :code
+end
+
 Neo4j::Transaction.run do
   stops_to_add.each do |stop|
-  	Stop.new(:name => stop[:name], :code => stop[:code])
+	node = Neo4j::Node.new(:name => stop[:name], :code => stop[:code], :type => "stop")
+  	puts "Code: #{stop[:code]}, Stop: #{stop[:name]}"
   end
 end
 
-p Stop.find(:code => 91532).first.name
+
+routes_file = data_dir + "routes.csv"
+routes = FasterCSV.read(routes_file).drop(1)
+
+bus_1_stops = routes.group_by { |route| route[0] }["1"].select { |route| route[1] == "1"  }.map { |route| route[4]  }
+
+stop_combinations = bus_1_stops.zip(bus_1_stops.drop(1))
+
+
+Neo4j::Transaction.run do
+	stop_combinations.each do |stop_combination|
+		p stop_combination[0]
+		unless stop_combination[1].nil?
+			stop1 = StopsIndex.find("code: #{stop_combination[0]}").first
+			stop2 = StopsIndex.find("code: #{stop_combination[1]}").first
+			p stop1, stop2			
+			Neo4j::Relationship.new(:route, stop1, stop2, { :bus_number => 1 })
+		end
+	end
+end
